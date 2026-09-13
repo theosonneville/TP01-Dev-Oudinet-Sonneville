a54e188a170365b9eaed3888038b3d22a6756909
Theo Sonneville <ton_email@example.com>
2026-09-13 22:48:08 +0200

    Affichage Addition

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
 public class Main {
-
+    public static void main(String[] args) {
+        System.out.println("Somme et Soustraction");
+        System.out.println("Additionn fonctionnelle dans Main");
+        
+    }
 }
